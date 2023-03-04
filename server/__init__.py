af55d0ea0dd1ad31370ed9f28da2149e295f584e
Sairaaj Surve <sairaaj26@gmail.com>
2023-03-04 21:42:58 +0530

    refactor folder structure

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -6,11 +6,13 @@ from api.analysis import routes as analysis_routes
 
 load_dotenv()
 
-app = Flask(__name__,
-            static_url_path='', 
-            static_folder='./public')
-# app = Flask(__name__)
+app = Flask(__name__)
+
 CORS(app, origins="*", supports_credentials=False)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 app.register_blueprint(analysis_routes.analysis_bp, url_prefix='/api/v1/analysis')
