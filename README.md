4f9fd443a9ddc52f7519d14e2d2ae37646adf257
Sairaaj Surve <sairaaj26@gmail.com>
2023-03-04 19:26:19 +0530

    add face-detector and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plus Plus
+
+## Script for creating Virtual Environment using pyenv-win
+
+```bash
+python -m venv .venv
+.\.venv\Scripts\activate
+pip install -r ./server/requirements.txt
+```
---
+++ server/lib/face-detector.py
@@ -0,0 +1,30 @@
+import cv2
+import mediapipe as mp
+import uuid
+
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+def borderBox(imageUrl):
+
+    with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detection:
+        image = cv2.imread(imageUrl)
+
+        # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+        results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        # Draw face detections of each face.
+        if not results.detections:
+            pass
+
+        annotated_image = image.copy()
+
+        for detection in results.detections:
+            mp_drawing.draw_detection(annotated_image, detection)
+
+        img_name = uuid.uuid4()
+
+        cv2.imwrite(f'../public/img/{img_name}.png', annotated_image)
+
+        return img_name
