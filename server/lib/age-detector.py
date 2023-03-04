dd95dff93483dc7845f7c5b8f1d1ca35d49ca468
Sairaaj Surve <sairaaj26@gmail.com>
2023-03-04 23:18:29 +0530

    fix age route

--- server/lib/age-detector.py
+++ server/lib/age-detector.py
@@ -1,16 +1,13 @@
 import requests
 import os
 
+
 API_URL = "https://api-inference.huggingface.co/models/nateraw/vit-age-classifier"
 headers = {"Authorization": f"Bearer {os.environ['HF_TOKEN']}"}
 
-def age_predictor(filename):
-    with open(filename, "rb") as f:
+def age_predictor(imgURL):
+    with open(imgURL, "rb") as f:
         data = f.read()
     response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
-
-img_file = "/content/amen.JPG"
-resp = age_predictor(img_file)
-age_pred = (resp[0])['label']
-age_pred
+    preds = response.json()
+    return preds
